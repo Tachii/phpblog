3234dffbcce93eda2a02d58cf18d86dd8a58d083
Tachi <zaveruha007@gmail.com>
2015-05-23 14:32:20 +0300

    Heroku release

--- config/config.php
+++ config/config.php
@@ -1,8 +1,8 @@
 <?php
-define('DB_HOST', 'mysql6.000webhost.com');
-define('DB_USER', 'a1713593_tachi');
-define('DB_PASS', 'a1713593');
-define('DB_NAME', 'a1713593_chat');   
+define('DB_HOST', 'us-cdbr-iron-east-02.cleardb.net');
+define('DB_USER', 'bb381fbfba17e9');
+define('DB_PASS', 'bfcbb838');
+define('DB_NAME', 'heroku_db3f597fb5d5c46');   
 
 $site_description = "Welcome to my blog. I made it to improve my skills in PHP. Feel free to explore and post your thoughts ;)";
 ?>
